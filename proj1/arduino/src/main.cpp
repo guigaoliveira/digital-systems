0b1cd1adfe082721b270a0ee0f0570884db8d94c
Guilherme Oliveira <gmbo182@hotmail.com>
2019-08-30 13:27:16 -0300

    ajustes no servidor e teste com a ponte h

--- proj1/arduino/src/main.cpp
+++ proj1/arduino/src/main.cpp
@@ -1,12 +1,25 @@
 #include <Arduino.h>
 
+int IN3 = 4;
+int IN4 = 5;
+int velocidadeB = 9;
 void setup()
 {
-  Serial.begin(9600);
+  //Define os pinos como saida
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+  pinMode(velocidadeB, OUTPUT);
 }
 
 void loop()
 {
-  Serial.write("hello \n");
-  delay(100);
+  //Gira o Motor A no sentido horario
+  analogWrite(velocidadeB, 255);
+  //Gira o Motor B no sentido horario
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+  delay(2000);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+  delay(2000);
 }
